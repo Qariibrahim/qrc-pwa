b6fe09d767524fc635d7a2ada574ddf73d2fdbba
Qariibrahim <qariibrahimishati@gmail.com>
2026-08-09 08:13:21 +0530

    New worker-new.js

--- worker-new.js
+++ worker-new.js
@@ -2362,7 +2362,7 @@ const TEST_DEVICE_ID =
 
 const singleDeviceTestDone =
   localStorage.getItem(
-    "irPwaSingleUpdateTestDone"
+    "irPwaSingleUpdateEmailTestDone"
   ) === "1";
 
 const currentVersion =
@@ -2602,7 +2602,7 @@ const currentVersion =
 
 if (currentDeviceId === TEST_DEVICE_ID) {
   localStorage.setItem(
-    "irPwaSingleUpdateTestDone",
+    "irPwaSingleUpdateEmailTestDone"
     "1"
   );
 }
